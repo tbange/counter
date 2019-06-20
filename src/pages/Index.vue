df392b99a3206617a8f65bac2ae1c634c332717b
tbange <dantbag.mukalay6@gmail.com>
2019-06-20 10:41:54 +0200

    Initial Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card-media>
+        <q-btn round color="primary" class="pull-right on-left">+</q-btn>
+        <q-btn round color="secondary" class="pull-right on-left">-</q-btn>
+    </q-card-media>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+
+
+</script>
